564e67779bb220ce47865dab8938871f8d0d392c
Marcos Ruiz <marcos.ruizmc05@gmail.com>
2026-09-02 19:13:12 -0300

    Le agrege unas cosas mas

--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,22 @@
 #include <Arduino.h>
 #include <WiFi.h>
 
+//Funciones
+void limpiarBuffer() {
+    while (Serial.available() > 0) {
+        Serial.read();
+    }
+}
+
 // Recordar borrar 
 const char* ssid = "Wokwi-GUEST";
 const char* password = "";
 
-IPAddress ip(192, 168, 0, 181);
-int puerto = 80;
+String targetIPStr = "";
+IPAddress targetIP;
+int puerto;
+int inicial;
+int fin;
 
 void setup() {
 
@@ -32,15 +42,47 @@ void setup() {
     Serial.print("[+] IP: ");
     Serial.println(WiFi.localIP());
 
+    Serial.println("\n ------------- CONFIGURACION ------------- ");
+
+    Serial.println("Introduce una ip (ejemplo: 192.168.0.3): ");
+    while (Serial.available() == 0){
+        delay(10);
+    }
+
+    targetIPStr = Serial.readStringUntil('\n'); //Lee todo el texto que escribiste en el Monitor Serie hasta que detecta un salto de línea (cuando presionas "Enviar").
+    targetIPStr.trim(); // elimina los espacios en blanco
+    targetIP.fromString(targetIPStr); // Convierte el String a formato IPAddress
+
+    Serial.println("Introduce el puerto inicial (ejemplo 80):");
+    while (Serial.available() == 0){
+        delay(10);
+    }
+    inicial = Serial.parseInt();
+    limpiarBuffer();
+
+    Serial.println("introduce el puerto final (ejemplo 1024):");
+    while (Serial.available() == 0){
+        delay(10);
+    }
+    fin = Serial.parseInt();
+    limpiarBuffer();
+    
+    Serial.println("[*] Scanner listo. Iniciando escaneo...\n");
     Serial.println("[*] Scanner listo.");
 
     WiFiClient client;
-
-    if (client.connect(ip, puerto)){
-        Serial.println("[OPEN] Puerto abierto");
-    } else {
-        Serial.println("[CLOSE] Puerto cerrado");
+for (int puerto = inicial; puerto <= fin; puerto++) {
+        if (client.connect(targetIP, puerto)) {
+            Serial.print("[OPEN] Puerto ");
+            Serial.println(puerto);
+            client.stop(); // Desconectar para liberar el socket
+        } else {
+            // Serial.print("[CLOSE] Puerto "); Serial.println(puerto);
+        }
+        delay(2);
     }
+
+    Serial.println("\n[*] Escaneo finalizado.");
 }
 
 void loop() {
